5a22bc48b630b6f1fa83378500ee3fb06ca863b1
Rfluid <ruy.vieiraneto@gmail.com>
2025-02-23 17:38:52 -0300

    feat: add main function logic at consumer

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	exchange_listener "github.com/reb-felipe/eventcounter/cmd/exchange-listener"
+	"github.com/reb-felipe/eventcounter/cmd/out"
+)
 
+func main() {
+	exchange_listener.Wait()
+	exchange_listener.ConsumeLoop()
+	out.GenerateConsumerOutputs()
 }
